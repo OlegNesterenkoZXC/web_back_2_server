0eec353b0dc4b0dd73fe67974c47d1b44503d154
Oleg <olegik-n@yandex.com>
2022-03-15 10:54:35 +0300

    Edit php file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Отправляем правильную кодировку.
-header('HTTP/1.1 200 ');
+header('HTTP/1.1 200 Ok');
 header('Content-Type: text/html; charset=UTF-8');
 
 // Выводим все полученные через POST параметры.
